1e0124aa6481c02c5e9843e7df55843e5f8685f4
Purvi Gangrade <62595622+purvigangrade@users.noreply.github.com>
2023-07-15 00:47:01 +0530

    Update script.js

--- Sixth_Assignment/script.js
+++ Sixth_Assignment/script.js
@@ -20,26 +20,26 @@
 
 //*************************************ANSWER - 1********************************************************** */
 
-// function findPermutation(s) {
-//     const n = s.length;
-//     const perm = new Array(n + 1);
-//     let currentNum = 1;
+function findPermutation(s) {
+    const n = s.length;
+    const perm = new Array(n + 1);
+    let currentNum = 1;
   
-//     for (let i = 0; i <= n; i++) {
-//       if (i === n || s[i] === 'I') {
-//         for (let j = i; j >= 0 && (i === n || s[j] === 'I'); j--) {
-//           perm[j] = currentNum++;
-//         }
-//       }
-//     }
+    for (let i = 0; i <= n; i++) {
+      if (i === n || s[i] === 'I') {
+        for (let j = i; j >= 0 && (i === n || s[j] === 'I'); j--) {
+          perm[j] = currentNum++;
+        }
+      }
+    }
   
-//     return perm;
-//   }
+    return perm;
+  }
   
-//   // Test case
-//   const s = "IDID";
-//   const perm = findPermutation(s);
-//   console.log(perm); // Output: [0, 4, 1, 3, 2]
+  // Test case
+  const s = "IDID";
+  const perm = findPermutation(s);
+  console.log(perm); // Output: [0, 4, 1, 3, 2]
 
 //******************************************************************************************************* */
 //  **Question 2**
@@ -60,34 +60,34 @@
 // **Output:** true
 
 //********************************ANSWER-2***************************************************************** */
-// function searchMatrix(matrix, target) {
-//     const m = matrix.length;
-//     const n = matrix[0].length;
+function searchMatrix(matrix, target) {
+    const m = matrix.length;
+    const n = matrix[0].length;
   
-//     let left = 0;
-//     let right = m * n - 1;
+    let left = 0;
+    let right = m * n - 1;
   
-//     while (left <= right) {
-//       const mid = Math.floor((left + right) / 2);
-//       const num = matrix[Math.floor(mid / n)][mid % n];
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      const num = matrix[Math.floor(mid / n)][mid % n];
   
-//       if (num === target) {
-//         return true;
-//       } else if (num < target) {
-//         left = mid + 1;
-//       } else {
-//         right = mid - 1;
-//       }
-//     }
+      if (num === target) {
+        return true;
+      } else if (num < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
   
-//     return false;
-//   }
+    return false;
+  }
   
-//   // Test case
-//   const matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]];
-//   const target = 3;
-//   const found = searchMatrix(matrix, target);
-//   console.log(found); // Output: true
+  // Test case
+  const matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]];
+  const target = 3;
+  const found = searchMatrix(matrix, target);
+  console.log(found); // Output: true
   
 //******************************************************************************************************* */
 //  **Question 3**
@@ -110,32 +110,32 @@
 // false
   
 //********************************ANSWER - 3*********************************************************************** */
-// function validMountainArray(arr) {
-//     const n = arr.length;
-//     let i = 0;
+function validMountainArray(arr) {
+    const n = arr.length;
+    let i = 0;
   
-//     // Check for increasing sequence
-//     while (i + 1 < n && arr[i] < arr[i + 1]) {
-//       i++;
-//     }
+    // Check for increasing sequence
+    while (i + 1 < n && arr[i] < arr[i + 1]) {
+      i++;
+    }
   
-//     // Check if peak is not at the first or last index
-//     if (i === 0 || i === n - 1) {
-//       return false;
-//     }
+    // Check if peak is not at the first or last index
+    if (i === 0 || i === n - 1) {
+      return false;
+    }
   
-//     // Check for decreasing sequence
-//     while (i + 1 < n && arr[i] > arr[i + 1]) {
-//       i++;
-//     }
+    // Check for decreasing sequence
+    while (i + 1 < n && arr[i] > arr[i + 1]) {
+      i++;
+    }
   
-//     return i === n - 1;
-//   }
+    return i === n - 1;
+  }
   
-//   // Test case
-//   const arr = [2, 1];
-//   const isValidMountain = validMountainArray(arr);
-//   console.log(isValidMountain); // Output: false
+  // Test case
+  const arr = [2, 1];
+  const isValidMountain = validMountainArray(arr);
+  console.log(isValidMountain); // Output: false
   
 //***************************************************************************************************************** */
 // <aside>
@@ -156,33 +156,33 @@
 // </aside>
 
 //********************************ANSWER - 4*********************************************************************** */
-// function findMaxLength(nums) {
-//     const map = new Map();
-//     map.set(0, -1);
-//     let maxLength = 0;
-//     let count = 0;
+function findMaxLength(nums) {
+    const map = new Map();
+    map.set(0, -1);
+    let maxLength = 0;
+    let count = 0;
   
-//     for (let i = 0; i < nums.length; i++) {
-//       if (nums[i] === 0) {
-//         count--;
-//       } else {
-//         count++;
-//       }
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 0) {
+        count--;
+      } else {
+        count++;
+      }
   
-//       if (map.has(count)) {
-//         maxLength = Math.max(maxLength, i - map.get(count));
-//       } else {
-//         map.set(count, i);
-//       }
-//     }
+      if (map.has(count)) {
+        maxLength = Math.max(maxLength, i - map.get(count));
+      } else {
+        map.set(count, i);
+      }
+    }
   
-//     return maxLength;
-//   }
+    return maxLength;
+  }
   
-//   // Test case
-//   const nums = [0, 1];
-//   const maxLength = findMaxLength(nums);
-//   console.log(maxLength); // Output: 2
+  // Test case
+  const nums = [0, 1];
+  const maxLength = findMaxLength(nums);
+  console.log(maxLength); // Output: 2
 //************************************************************************************************************* */
 
 // <aside>
@@ -207,24 +207,24 @@
 // </aside>
 
 //**************************************ANSWER - 5************************************************************** */
-// function minProductSum(nums1, nums2) {
-//     nums1.sort((a, b) => a - b); // Sort nums1 in ascending order
-//     nums2.sort((a, b) => b - a); // Sort nums2 in descending order
+function minProductSum(nums1, nums2) {
+    nums1.sort((a, b) => a - b); // Sort nums1 in ascending order
+    nums2.sort((a, b) => b - a); // Sort nums2 in descending order
   
-//     let minProductSum = 0;
+    let minProductSum = 0;
   
-//     for (let i = 0; i < nums1.length; i++) {
-//       minProductSum += nums1[i] * nums2[i];
-//     }
+    for (let i = 0; i < nums1.length; i++) {
+      minProductSum += nums1[i] * nums2[i];
+    }
   
-//     return minProductSum;
-//   }
+    return minProductSum;
+  }
   
-//   // Test case
-//   const nums1 = [5, 3, 4, 2];
-//   const nums2 = [4, 2, 2, 5];
-//   const result = minProductSum(nums1, nums2);
-//   console.log(result); // Output: 40
+  // Test case
+  const nums1 = [5, 3, 4, 2];
+  const nums2 = [4, 2, 2, 5];
+  const result = minProductSum(nums1, nums2);
+  console.log(result); // Output: 40
   
 //************************************************************************************************************* */
 // <aside>
@@ -251,43 +251,43 @@
 // </aside>
 
 //**************************************ANSWER - 6************************************************************** */
-// function findOriginalArray(changed) {
-//     if (changed.length % 2 !== 0) {
-//       return [];
-//     }
+function findOriginalArray(changed) {
+    if (changed.length % 2 !== 0) {
+      return [];
+    }
   
-//     const original = [];
-//     const numCounts = new Map();
+    const original = [];
+    const numCounts = new Map();
   
-//     for (let num of changed) {
-//       numCounts.set(num, (numCounts.get(num) || 0) + 1);
-//     }
+    for (let num of changed) {
+      numCounts.set(num, (numCounts.get(num) || 0) + 1);
+    }
   
-//     changed.sort((a, b) => a - b);
+    changed.sort((a, b) => a - b);
   
-//     for (let num of changed) {
-//       if (numCounts.get(num) === 0) {
-//         continue;
-//       }
+    for (let num of changed) {
+      if (numCounts.get(num) === 0) {
+        continue;
+      }
   
-//       const doubleNum = num * 2;
+      const doubleNum = num * 2;
   
-//       if (!numCounts.has(doubleNum) || numCounts.get(doubleNum) === 0) {
-//         return [];
-//       }
+      if (!numCounts.has(doubleNum) || numCounts.get(doubleNum) === 0) {
+        return [];
+      }
   
-//       original.push(num);
-//       numCounts.set(num, numCounts.get(num) - 1);
-//       numCounts.set(doubleNum, numCounts.get(doubleNum) - 1);
-//     }
+      original.push(num);
+      numCounts.set(num, numCounts.get(num) - 1);
+      numCounts.set(doubleNum, numCounts.get(doubleNum) - 1);
+    }
   
-//     return original;
-//   }
+    return original;
+  }
   
-//   // Test case
-//   const changed = [1, 3, 4, 2, 6, 8];
-//   const result = findOriginalArray(changed);
-//   console.log(result); // Output: [1, 3, 4]
+  // Test case
+  const changed = [1, 3, 4, 2, 6, 8];
+  const result = findOriginalArray(changed);
+  console.log(result); // Output: [1, 3, 4]
   
 //************************************************************************************************************* */
 //  **Question 7**
@@ -300,52 +300,52 @@
 // **Output:** [[1,2,3],[8,9,4],[7,6,5]]
 
 //**************************************ANSWER - 7************************************************************** */
-// function generateMatrix(n) {
-//     const matrix = new Array(n).fill().map(() => new Array(n).fill(0));
-//     let num = 1;
-//     let topRow = 0;
-//     let bottomRow = n - 1;
-//     let leftCol = 0;
-//     let rightCol = n - 1;
+function generateMatrix(n) {
+    const matrix = new Array(n).fill().map(() => new Array(n).fill(0));
+    let num = 1;
+    let topRow = 0;
+    let bottomRow = n - 1;
+    let leftCol = 0;
+    let rightCol = n - 1;
   
-//     while (topRow <= bottomRow && leftCol <= rightCol) {
-//       // Traverse right
-//       for (let col = leftCol; col <= rightCol; col++) {
-//         matrix[topRow][col] = num++;
-//       }
-//       topRow++;
+    while (topRow <= bottomRow && leftCol <= rightCol) {
+      // Traverse right
+      for (let col = leftCol; col <= rightCol; col++) {
+        matrix[topRow][col] = num++;
+      }
+      topRow++;
   
-//       // Traverse down
-//       for (let row = topRow; row <= bottomRow; row++) {
-//         matrix[row][rightCol] = num++;
-//       }
-//       rightCol--;
+      // Traverse down
+      for (let row = topRow; row <= bottomRow; row++) {
+        matrix[row][rightCol] = num++;
+      }
+      rightCol--;
   
-//       // Traverse left
-//       if (topRow <= bottomRow) {
-//         for (let col = rightCol; col >= leftCol; col--) {
-//           matrix[bottomRow][col] = num++;
-//         }
-//         bottomRow--;
-//       }
+      // Traverse left
+      if (topRow <= bottomRow) {
+        for (let col = rightCol; col >= leftCol; col--) {
+          matrix[bottomRow][col] = num++;
+        }
+        bottomRow--;
+      }
   
-//       // Traverse up
-//       if (leftCol <= rightCol) {
-//         for (let row = bottomRow; row >= topRow; row--) {
-//           matrix[row][leftCol] = num++;
-//         }
-//         leftCol++;
-//       }
-//     }
+      // Traverse up
+      if (leftCol <= rightCol) {
+        for (let row = bottomRow; row >= topRow; row--) {
+          matrix[row][leftCol] = num++;
+        }
+        leftCol++;
+      }
+    }
   
-//     return matrix;
-//   }
+    return matrix;
+  }
   
-//   // Test case
-//   const n = 3;
-//   const result = generateMatrix(n);
-//   console.log(result);
-//   // Output: [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
+  // Test case
+  const n = 3;
+  const result = generateMatrix(n);
+  console.log(result);
+  // Output: [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
   
 //*************************************************************************************************************** */
 //  **Question 8**
