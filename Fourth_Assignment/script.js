183899e8bed612b2bd9333b3c7c2380be59e5840
Purvi Gangrade <62595622+purvigangrade@users.noreply.github.com>
2023-07-15 00:43:47 +0530

    Update script.js

--- Fourth_Assignment/script.js
+++ Fourth_Assignment/script.js
@@ -9,36 +9,36 @@
 // **Explanation:** Only 1 and 5 appeared in the three arrays.
 
 //***************************ANSWER -1************************************************************** */
-// function arraysIntersection(arr1, arr2, arr3) {
-//     const result = [];
-//     let i = 0;
-//     let j = 0;
-//     let k = 0;
+function arraysIntersection(arr1, arr2, arr3) {
+    const result = [];
+    let i = 0;
+    let j = 0;
+    let k = 0;
   
-//     while (i < arr1.length && j < arr2.length && k < arr3.length) {
-//       if (arr1[i] === arr2[j] && arr2[j] === arr3[k]) {
-//         result.push(arr1[i]);
-//         i++;
-//         j++;
-//         k++;
-//       } else if (arr1[i] < arr2[j]) {
-//         i++;
-//       } else if (arr2[j] < arr3[k]) {
-//         j++;
-//       } else {
-//         k++;
-//       }
-//     }
+    while (i < arr1.length && j < arr2.length && k < arr3.length) {
+      if (arr1[i] === arr2[j] && arr2[j] === arr3[k]) {
+        result.push(arr1[i]);
+        i++;
+        j++;
+        k++;
+      } else if (arr1[i] < arr2[j]) {
+        i++;
+      } else if (arr2[j] < arr3[k]) {
+        j++;
+      } else {
+        k++;
+      }
+    }
   
-//     return result;
-//   }
+    return result;
+  }
   
-//   // Test case
-//   const arr1 = [1, 2, 3, 4, 5];
-//   const arr2 = [1, 2, 5, 7, 9];
-//   const arr3 = [1, 3, 4, 5, 8];
-//   const intersection = arraysIntersection(arr1, arr2, arr3);
-//   console.log(intersection); // Output: [1, 5]  
+  // Test case
+  const arr1 = [1, 2, 3, 4, 5];
+  const arr2 = [1, 2, 5, 7, 9];
+  const arr3 = [1, 3, 4, 5, 8];
+  const intersection = arraysIntersection(arr1, arr2, arr3);
+  console.log(intersection); // Output: [1, 5]  
 //************************************************************************************************** */
 // **Question 2**
 
@@ -55,33 +55,33 @@
 
 // **Output:** [[1,3],[4,6]]
 //***************************ANSWER -2************************************************************** */
-// function distinctNumbers(nums1, nums2) {
-//     const set1 = new Set(nums1);
-//     const set2 = new Set(nums2);
+function distinctNumbers(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
   
-//     const notInNums2 = [];
-//     const notInNums1 = [];
+    const notInNums2 = [];
+    const notInNums1 = [];
   
-//     for (const num of set1) {
-//       if (!set2.has(num)) {
-//         notInNums2.push(num);
-//       }
-//     }
+    for (const num of set1) {
+      if (!set2.has(num)) {
+        notInNums2.push(num);
+      }
+    }
   
-//     for (const num of set2) {
-//       if (!set1.has(num)) {
-//         notInNums1.push(num);
-//       }
-//     }
+    for (const num of set2) {
+      if (!set1.has(num)) {
+        notInNums1.push(num);
+      }
+    }
   
-//     return [notInNums2, notInNums1];
-//   }
+    return [notInNums2, notInNums1];
+  }
   
-//   // Test case
-//   const nums1 = [1, 2, 3];
-//   const nums2 = [2, 4, 6];
-//   const answer = distinctNumbers(nums1, nums2);
-//   console.log(answer); // Output: [[1, 3], [4, 6]]  
+  // Test case
+  const nums1 = [1, 2, 3];
+  const nums2 = [2, 4, 6];
+  const answer = distinctNumbers(nums1, nums2);
+  console.log(answer); // Output: [[1, 3], [4, 6]]  
 //************************************************************************************************** */
 // **Question 3**
 // Given a 2D integer array matrix, return *the **transpose** of* matrix.
@@ -95,26 +95,26 @@
 // Output: [[1,4,7],[2,5,8],[3,6,9]]
 
 //***************************ANSWER -3************************************************************** */
-// function transpose(matrix) {
-//     const rows = matrix.length;
-//     const cols = matrix[0].length;
+function transpose(matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
   
-//     const transposed = [];
-//     for (let j = 0; j < cols; j++) {
-//       const row = [];
-//       for (let i = 0; i < rows; i++) {
-//         row.push(matrix[i][j]);
-//       }
-//       transposed.push(row);
-//     }
+    const transposed = [];
+    for (let j = 0; j < cols; j++) {
+      const row = [];
+      for (let i = 0; i < rows; i++) {
+        row.push(matrix[i][j]);
+      }
+      transposed.push(row);
+    }
   
-//     return transposed;
-//   }
+    return transposed;
+  }
   
-//   // Test case
-//   const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-//   const transposedMatrix = transpose(matrix);
-//   console.log(transposedMatrix); // Output: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]  
+  // Test case
+  const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  const transposedMatrix = transpose(matrix);
+  console.log(transposedMatrix); // Output: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]  
 //************************************************************************************************** */
 //  **Question 4**
 // Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is **maximized**. Return *the maximized sum*.
@@ -136,21 +136,21 @@
 // So the maximum possible sum is 4.
 
 //************************************ANSWER - 4************************************************************** */
-// function arrayPairSum(nums) {
-//     nums.sort((a, b) => a - b);
+function arrayPairSum(nums) {
+    nums.sort((a, b) => a - b);
   
-//     let sum = 0;
-//     for (let i = 0; i < nums.length; i += 2) {
-//       sum += nums[i];
-//     }
+    let sum = 0;
+    for (let i = 0; i < nums.length; i += 2) {
+      sum += nums[i];
+    }
   
-//     return sum;
-//   }
+    return sum;
+  }
   
-//   // Test case
-//   const nums = [1, 4, 3, 2];
-//   const maxSum = arrayPairSum(nums);
-//   console.log(maxSum); // Output: 4  
+  // Test case
+  const nums = [1, 4, 3, 2];
+  const maxSum = arrayPairSum(nums);
+  console.log(maxSum); // Output: 4  
 //************************************************************************************************** */
 // **Question 5**
 // You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase **may be** incomplete.
@@ -166,23 +166,23 @@
 
 //*****************************************ANSWER - 5********************************************************* */
 
-// function arrangeCoins(n) {
-//     let completeRows = 0;
-//     let row = 1;
+function arrangeCoins(n) {
+    let completeRows = 0;
+    let row = 1;
   
-//     while (n >= row) {
-//       n -= row;
-//       row++;
-//       completeRows++;
-//     }
+    while (n >= row) {
+      n -= row;
+      row++;
+      completeRows++;
+    }
   
-//     return completeRows;
-//   }
+    return completeRows;
+  }
   
-//   // Test case
-//   const n = 5;
-//   const completeRows = arrangeCoins(n);
-//   console.log(completeRows); // Output: 2  
+  // Test case
+  const n = 5;
+  const completeRows = arrangeCoins(n);
+  console.log(completeRows); // Output: 2  
 //************************************************************************************************** */
 //  **Question 6**
 // Given an integer array nums sorted in **non-decreasing** order, return *an array of **the squares of each number** sorted in non-decreasing order*.
@@ -197,16 +197,16 @@
 // After sorting, it becomes [0,1,9,16,100]
 
 //**************************************ANSWER-6************************************************************ */
-// function sortedSquares(nums) {
-//     const squares = nums.map(num => num * num);
-//     squares.sort((a, b) => a - b);
-//     return squares;
-//   }
+function sortedSquares(nums) {
+    const squares = nums.map(num => num * num);
+    squares.sort((a, b) => a - b);
+    return squares;
+  }
   
-//   // Test case
-//   const nums = [-4, -1, 0, 3, 10];
-//   const sortedSquaresArray = sortedSquares(nums);
-//   console.log(sortedSquaresArray); // Output: [0, 1, 9, 16, 100]
+  // Test case
+  const nums = [-4, -1, 0, 3, 10];
+  const sortedSquaresArray = sortedSquares(nums);
+  console.log(sortedSquaresArray); // Output: [0, 1, 9, 16, 100]
   
 //************************************************************************************************** */
 //  **Question 7**
@@ -222,24 +222,24 @@
 // **Explanation:** The maximum integer in M is 2, and there are four of it in M. So return 4.
 
 //**************************************ANSWER-7************************************************************ */
-// function maxCount(m, n, ops) {
-//     let minRow = m;
-//     let minCol = n;
+function maxCount(m, n, ops) {
+    let minRow = m;
+    let minCol = n;
   
-//     for (const [row, col] of ops) {
-//       minRow = Math.min(minRow, row);
-//       minCol = Math.min(minCol, col);
-//     }
+    for (const [row, col] of ops) {
+      minRow = Math.min(minRow, row);
+      minCol = Math.min(minCol, col);
+    }
   
-//     return minRow * minCol;
-//   }
+    return minRow * minCol;
+  }
   
-//   // Test case
-//   const m = 3;
-//   const n = 3;
-//   const ops = [[2, 2], [3, 3]];
-//   const maxIntegers = maxCount(m, n, ops);
-//   console.log(maxIntegers); // Output: 4
+  // Test case
+  const m = 3;
+  const n = 3;
+  const ops = [[2, 2], [3, 3]];
+  const maxIntegers = maxCount(m, n, ops);
+  console.log(maxIntegers); // Output: 4
   
 //******************************************************************************************************* */
 //  **Question 8**
